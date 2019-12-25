e69ba7b142d402082ba3e5a3b2b7a3137de0c7fa
aquila57 <58740691+aquila57@users.noreply.github.com>
2019-12-25 11:40:49 -0600

    Add files via upload

---
+++ deinst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make -f runsb.mak clean
+make -f runsfibo.mak clean
+make -f runslfsr.mak clean
+make -f runsine.mak clean
+make -f runsgsl.mak clean
+make -f ruruns.mak clean
+rm -f allgsl.lst
---
+++ inst.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+make -f runsb.mak
+make -f runsfibo.mak
+make -f runslfsr.mak
+make -f runsine.mak
+make -f runsgsl.mak
+make -f ruruns.mak
